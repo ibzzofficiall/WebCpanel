f87a21450549650d4695dc7464286c70495fa769
ibzzofficiall <ibzzofficiall29@gmail.com>
2026-04-12 06:14:47 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
- TELEGRAM_TOKEN: '8381993210:AAFQpJgtFNHxVPuqCR4nizw0fQAk20vaI0A', //isi bot token kalian
- ADMIN_ID: '7903962929', // isi id kalin
- VERCEL_TOKEN: '34CCU7k76SxeaQMz1YqbcAaW',
- BOT_USERNAME: 'Ranzzmdnew_bot', // isi username bot kalian jangan pakek @ langsung namanya aja
- };
